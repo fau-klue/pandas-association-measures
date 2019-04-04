66b0bf0b4e357384ae71b3c4114d471c1b55e3a0
Markus Opolka <markus@martialblog.de>
2019-04-04 15:15:24 +0200

    Column calculation (#1)
    *  Testing column-wise calculation

--- association/measures.py
+++ association/measures.py
@@ -9,44 +9,66 @@ import pandas as pd
 import numpy as np
 
 
-def contingency_table(row):
+def contingency_table(df):
     """
     Calculate contingency table for observed data.
     f1 = R1 = O11 + O12 (Anzahl Vorkommen von t1)
     f2 = C1 = O11 + O21 (Anzahl Vorkommen von t2)
     O11 = O (Anzahl gemeinsame Vorkommen von t1 und t2)
     N: Number of tokens in corpus
-    :param pandas.Series row: Pandas Series containing f1, f2, O11 and N
-    :return: pandas.Series containing O12, O21, O22
-    :rtype: pandas.Series
+    :return: Tuple of pandas.Series
+    :rtype: tuple
     """
-    O12 = row['f1'] - row['O11']
-    O21 = row['f2'] - row['O11']
-    O22 = row['N'] - (row['f1'] + row['f2'] + row['O11'])
-    return pd.Series([O12, O21, O22])
+
+    O12 = pd.Series(data=df['f1'] - df['O11'])
+    O21 = pd.Series(data=df['f2'] - df['O11'])
+    O22 = pd.Series(data=df['N'] - (df['f1'] + df['f2'] + df['O11']))
+
+    return (df['O11'], O12, O21, O22)
 
 
-def expected_frequencies(row):
+def expected_frequencies(df):
     """
     Calculate expected frequencies for observed frequencies.
     """
-    # TODO: Should I do this in one step with the other data to avoid another iteration?
-    return (row['f1'] * row['f2']) / row['N']
+
+    if 'O12' not in df.columns:
+        df['O11'], df['O12'], df['O21'], df['O22'] =contingency_table(df)
+
+    R1 = df['f1'] # f1 Frequency
+    C1 = df['f2'] # f2 Frequency
+    R2 = df['O21'] + df['O22']
+    C2 = df['O12'] + df['O22']
+
+    E11 = (R1 * C1) / df['N']
+    E12 = (R1 * C2) / df['N']
+    E21 = (R2 * C1) / df['N']
+    E22 = (R2 * C2) / df['N']
+
+    return (E11, E12, E21, E22)
 
 
-def z_score(row):
+def z_score(df):
     """
     Calculate z-score
     """
-    if 'E11' not in row.index:
+
+    if 'E11' not in df.columns:
         return np.nan
-    return (row['O11'] - row['E11']) / np.sqrt(row['E11'])
+
+    res = (df['O11'] - df['E11']) / np.sqrt(df['E11'])
+
+    return pd.Series(data=res)
 
 
-def mutual_information(row):
+def mutual_information(df):
     """
     Calculate Mutual Information
     """
-    if 'E11' not in row.index:
+
+    if 'E11' not in df.columns:
         return np.nan
-    return np.log2(row['O11'] / row['E11'])
+
+    res = np.log2(df['O11'] / df['E11'])
+
+    return pd.Series(data=res)
--- performance.py
+++ performance.py
@@ -3,33 +3,51 @@ Script to measure the performance
 """
 
 import timeit
+import sys
 
 # Number of iterations
 iterations = 1000
+if len(sys.argv) > 1:
+    iterations = int(sys.argv[1])
 
 # code snippet to be executed only once
 setup = """
 import pandas as pd
 import numpy as np
-import association.measures
+import association.measures as am
 
 df = pd.DataFrame({'f1': np.random.randint(100, size=10),
                    'f2': np.random.randint(100, size=10),
                    'O11': np.random.randint(100, size=10),
                    'N': [100] * 10})
+
+df['O11'], df['O12'], df['O21'], df['O22'] = am.contingency_table(df)
 """
 
 # code snippet whose execution time is to be measured
-code1 = '''
-df.apply(association.measures.expected_frequencies, axis=1)
+code_1 = '''
+df['O11'], df['O12'], df['O21'], df['O22'] = am.contingency_table(df)
 '''
 
-code2 = '''
-df.apply(association.measures.contingency_table, axis=1)
+code_2 = '''
+df['E11'], df['E12'], df['E21'], df['E22'] = am.expected_frequencies(df)
 '''
 
-res1 = timeit.timeit(setup=setup, stmt=code1, number=iterations)
-res2 = timeit.timeit(setup=setup, stmt=code2, number=iterations)
+code_3 = '''
+df['E11'], df['E12'], df['E21'], df['E22'] = am.expected_frequencies(df)
+df['am'] = am.z_score(df)
+'''
 
-print('Calculate expected_frequencies (iterations={iter}): {res}'.format(iter=iterations, res=res1))
-print('Calculate contingency_table (iterations={iter}): {res}'.format(iter=iterations, res=res2))
+code_4 = '''
+df['E11'], df['E12'], df['E21'], df['E22'] = am.expected_frequencies(df)
+df['am'] = am.mutual_information(df)
+'''
+
+res1 = timeit.timeit(setup=setup, stmt=code_1, number=iterations)
+print('Calculate contingency_table (iterations={iter}): {res}'.format(iter=iterations, res=res1))
+res2 = timeit.timeit(setup=setup, stmt=code_2, number=iterations)
+print('Calculate expected_frequencies (iterations={iter}): {res}'.format(iter=iterations, res=res2))
+res3 = timeit.timeit(setup=setup, stmt=code_3, number=iterations)
+print('Calculate z_score (iterations={iter}): {res}'.format(iter=iterations, res=res3))
+res4 = timeit.timeit(setup=setup, stmt=code_4, number=iterations)
+print('Calculate MI (iterations={iter}): {res}'.format(iter=iterations, res=res4))
