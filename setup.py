a496a35712ec5deca6a49445405acb34ce80db77
Markus Opolka <markus@martialblog.de>
2019-04-05 18:51:23 +0200

    Add Twine for PyPi upload

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
+#!/usr/bin/env python3
+
+
 import os
 import sys
-from setuptools import setup
+from setuptools import find_packages, setup, Command
+
 
 # Package meta-data.
 NAME = 'association-measures'
@@ -15,8 +19,8 @@ REQUIRED = [
     'pandas'
 ]
 
-
 here = os.path.abspath(os.path.dirname(__file__))
+
 
 # Import the README and use it as the long-description.
 try:
@@ -33,6 +37,43 @@ if not VERSION:
         exec(f.read(), about)
 else:
     about['__version__'] = VERSION
+
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(here, 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPI via Twine…')
+        os.system('twine upload dist/*')
+
+        self.status('Pushing git tags…')
+        os.system('git tag v{0}'.format(about['__version__']))
+        os.system('git push --tags')
+
+        sys.exit()
 
 setup(
     name=NAME,
