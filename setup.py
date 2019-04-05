831e6e91b7b89eca8e0bec45341a4cbc2057c47a
Markus Opolka <markus@martialblog.de>
2019-04-05 17:51:46 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,55 @@
+import os
+import sys
+from setuptools import setup
+
+# Package meta-data.
+NAME = 'association-measures'
+DESCRIPTION = 'Corpus association measures for Python pandas'
+URL = 'https://github.com/fau-klue/pandas-association-measures'
+EMAIL = 'markus@martialblog.de'
+AUTHOR = 'Markus Opolka'
+REQUIRES_PYTHON = '>=3.6.0'
+VERSION = None
+
+REQUIRED = [
+    'pandas'
+]
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Import the README and use it as the long-description.
+try:
+    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+        long_description = '\n' + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+# Load the package's __version__.py module as a dictionary.
+about = {}
+if not VERSION:
+    project_slug = NAME.lower().replace("-", "_").replace(" ", "_")
+    with open(os.path.join(here, project_slug, '__version__.py')) as f:
+        exec(f.read(), about)
+else:
+    about['__version__'] = VERSION
+
+setup(
+    name=NAME,
+    version=about['__version__'],
+    description=DESCRIPTION,
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=find_packages(exclude=["tests", "test_*"]),
+    install_requires=REQUIRED,
+    include_package_data=True,
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
