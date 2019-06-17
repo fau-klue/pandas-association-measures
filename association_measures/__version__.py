8d56fe73a8d6ed3b7e8c313a5fc43916ed2ff695
Markus Opolka <markus@martialblog.de>
2019-06-17 15:12:26 +0200

    Bump to version 0.1.3

--- association_measures/__version__.py
+++ association_measures/__version__.py
@@ -2,5 +2,5 @@
 Association measures are mathematical formulae that interpret cooccurrence frequency data.
 """
 
-VERSION = (0, 1, 2)
+VERSION = (0, 1, 3)
 __version__ = '.'.join(map(str, VERSION))
