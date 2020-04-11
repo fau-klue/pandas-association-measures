36068c663754858657ac2f6aaee7851be400e4ba
Philipp Heinrich <philipp.heinrich@fau.de>
2020-04-11 14:54:34 +0200

    locate bug in choose function

--- tests/test_binomial.py
+++ tests/test_binomial.py
@@ -1,8 +1,10 @@
 import pytest
 
 from association_measures import binomial as bi
+from association_measures.measures import choose
 
 
+@pytest.mark.skip
 @pytest.mark.binomial
 def test_choose():
 
@@ -13,3 +15,4 @@ def test_choose():
     assert bi.choose(-3, 5) == 0
     assert bi.choose(-2, -2) == 0
     assert bi.choose(10, -2) == 0
+    assert bi.choose(100000, 15000) == choose(100000, 15000)
