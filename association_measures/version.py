a6ec8ff0e90b0649a1599df4446fc70924f90377
Philipp Heinrich <philipp.heinrich@fau.de>
2022-08-30 17:45:35 +0200

    bump version

--- association_measures/version.py
+++ association_measures/version.py
@@ -2,5 +2,5 @@
 Association measures are mathematical formulae that interpret cooccurrence frequency data.
 """
 
-VERSION = (0, 2, 3)
+VERSION = (0, 2, 4)
 __version__ = '.'.join(map(str, VERSION))
